d78e911d934ecfbecd2bb37897e6be0a4eddf8df
IvanTorresMia <ivantorresmia27@gmail.com>
2023-12-16 18:19:21 -0800

    server

---
+++ api/server/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import morgan from "morgan";
+import { db } from "../models";
+import userRoutes from "../routes/userRoutes";
+import todoRoutes from "../routes/todoRoutes";
+
+const app = express();
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/user", userRoutes);
+app.use("/api/todo", todoRoutes);
+
+const server = http.createServer(app);
+
+db.sync({ alter: true }).then(() => {
+    console.info("connected to DB!");
+});
+
+server.listen(3072, () => {
+    console.log("Server Running one 3072");
+});
