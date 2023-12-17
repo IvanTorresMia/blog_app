85a3c304acd835fd4653fa3e30239ac4d87475a6
IvanTorresMia <ivantorresmia27@gmail.com>
2023-12-16 18:19:26 -0800

    auth

---
+++ api/services/auth.ts
@@ -0,0 +1,41 @@
+import bcrypt from "bcrypt";
+import { Request } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
+
+const secret = "todopated";
+
+export const hashPassword = async (planTextPassword: string) => {
+    const saltRound = 12;
+    const hash = await bcrypt.hash(planTextPassword, saltRound);
+    return hash;
+};
+
+export const comparePasswords = async (
+    planTextPassword: string,
+    hashPassword: string
+) => {
+    return await bcrypt.compare(planTextPassword, hashPassword);
+};
+
+export const signUserToken = async (user: User) => {
+    let token = jwt.sign({ userId: user.userId }, secret, { expiresIn: "1hr" });
+    return token;
+};
+
+export const verifyUser = async (req: Request) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+
+        try {
+            let decoded: any = await jwt.verify(token, secret);
+            return User.findByPk(decoded.userId);
+        } catch (err) {
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
