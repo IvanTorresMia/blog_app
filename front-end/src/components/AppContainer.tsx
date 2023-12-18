c51adeae53e0c2949b5850a898299b8e1c089352
IvanTorresMia <ivantorresmia27@gmail.com>
2023-12-17 17:51:12 -0800

    app container

---
+++ front-end/src/components/AppContainer.tsx
@@ -0,0 +1,24 @@
+import { Box, Grid, ThemeProvider } from "@mui/material";
+import React, { ReactNode } from "react";
+import { theme } from "../theme";
+import { SnackbarProvider } from "notistack";
+
+interface IProps {
+    children: ReactNode;
+}
+
+export default function AppContainer({ children }: IProps) {
+    return (
+        <ThemeProvider theme={theme}>
+            <SnackbarProvider>
+                <Box
+                    display="flex"
+                    justifyContent={"space-between"}
+                    alignItems="center"
+                >
+                    {children}
+                </Box>
+            </SnackbarProvider>
+        </ThemeProvider>
+    );
+}
