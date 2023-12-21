d9b59a6d57b7a6318f73a7bbf183f78b965c22de
IvanTorresMia <ivantorresmia27@gmail.com>
2023-12-21 14:38:51 -0800

    remove padding

--- front-end/src/pages/home/Home.tsx
+++ front-end/src/pages/home/Home.tsx
@@ -1,14 +1,8 @@
 import { Grid, Typography } from "@mui/material";
-import { theme } from "../../theme";
 
 export default function Home() {
     return (
-        <Grid
-            container
-            paddingTop={theme.spacing(6)}
-            paddingLeft={theme.spacing(15)}
-            paddingRight={theme.spacing(15)}
-        >
+        <Grid container>
             <Grid item xs={12}>
                 <Typography variant="h1">Welcome Back</Typography>
             </Grid>
