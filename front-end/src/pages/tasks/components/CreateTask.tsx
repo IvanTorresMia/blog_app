8748cd9d6b65d511d0e19c20cdedf1e49a562884
IvanTorresMia <ivantorresmia27@gmail.com>
2023-12-21 14:39:05 -0800

    create tasks page

---
+++ front-end/src/pages/tasks/components/CreateTask.tsx
@@ -0,0 +1,76 @@
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import {
+    Controller,
+    FieldValues,
+    SubmitHandler,
+    useForm,
+} from "react-hook-form";
+import { createNewTask } from "../../../apis/app_apis";
+import { useEffect } from "react";
+
+interface IProps {
+    updateTasks: () => void;
+}
+
+export default function CreateTask({ updateTasks }: IProps) {
+    const {
+        control,
+        handleSubmit,
+        reset,
+        formState: { errors, isSubmitting, isSubmitSuccessful },
+    } = useForm();
+
+    const onSubmitValid: SubmitHandler<FieldValues> = async (data) => {
+        if (isSubmitting) return;
+
+        try {
+            await createNewTask({ message: data.task });
+            updateTasks();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        reset({
+            task: "",
+        });
+    }, [isSubmitSuccessful]);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+            <Grid container spacing={2} justifyContent={"center"}>
+                <Grid item xs={7}>
+                    <Typography variant="subtitle2">Create a task</Typography>
+                </Grid>
+                <Grid item xs={7}>
+                    <Controller
+                        name="task"
+                        control={control}
+                        rules={{ required: "Task is required" }}
+                        render={({ field: { ref, ...field } }) => (
+                            <TextField
+                                {...field}
+                                inputRef={ref}
+                                error={!!errors.task}
+                                label="Task"
+                                type="text"
+                                variant="outlined"
+                                fullWidth
+                            />
+                        )}
+                    />
+                </Grid>
+                <Grid item xs={7}>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        style={{ textTransform: "none" }}
+                    >
+                        Add new task
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>
+    );
+}
