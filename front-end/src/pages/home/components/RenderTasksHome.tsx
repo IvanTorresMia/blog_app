d89af4b93a42d0a6996a1b6ab4bfeed6e5945009
IvanTorresMia <ivantorresmia27@gmail.com>
2023-12-31 00:45:45 -0800

    add last updated

--- front-end/src/pages/home/components/RenderTasksHome.tsx
+++ front-end/src/pages/home/components/RenderTasksHome.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import { getAllUserTasks } from "../../../apis/app_apis";
 import { User } from "../../../types/auth-types";
 import { GetTask } from "../../../types/app-types";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Divider, Grid, Typography } from "@mui/material";
 import { theme } from "../../../theme";
 import { lightGray } from "../../../const/colors";
+import { formatDate } from "../../../utils/dateUtils";
 
 interface IProps {
     user: User;
@@ -39,16 +40,36 @@ export default function RenderTasksHome({ user }: IProps) {
                             border={`${theme.spacing(0.2)} solid ${lightGray}`}
                             borderRadius={1}
                         >
-                            <Typography variant="subtitle2">
+                            <Typography
+                                variant="subtitle1"
+                                paddingBottom={theme.spacing(2)}
+                            >
                                 {task.message}
                             </Typography>
-                            <Box>
-                                <Typography variant="caption">
-                                    Created on
-                                </Typography>
-                                <Typography variant="subtitle2">
-                                    {task.createdAt}
-                                </Typography>
+                            <Divider />
+                            <Box display={"flex"} paddingTop={theme.spacing(2)}>
+                                <Box>
+                                    <Typography variant="caption">
+                                        Created on
+                                    </Typography>
+                                    <Typography variant="subtitle2">
+                                        {formatDate(task.createdAt)}
+                                    </Typography>
+                                </Box>
+                                <Box
+                                    paddingRight={theme.spacing(2)}
+                                    paddingLeft={theme.spacing(2)}
+                                >
+                                    <Divider orientation="vertical" />
+                                </Box>
+                                <Box>
+                                    <Typography variant="caption">
+                                        Last updated
+                                    </Typography>
+                                    <Typography variant="subtitle2">
+                                        {formatDate(task.updatedAt)}
+                                    </Typography>
+                                </Box>
                             </Box>
                         </Grid>
                     );
